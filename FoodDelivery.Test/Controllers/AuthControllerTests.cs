ebc66d44f15bc5c3e4526f11af1bad136830792a
Dhanvitha <dhanvithapagidipally@gmail.com>
2026-06-01 10:36:33 +0530

    Completed backend project

---
+++ FoodDelivery.Test/Controllers/AuthControllerTests.cs
@@ -0,0 +1,99 @@
+﻿using Xunit;
+using FoodDelivery.Controllers;
+using FoodDelivery.Data;
+using FoodDelivery.DTOs;
+using FoodDelivery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.ComponentModel;
+
+namespace FoodDelivery.Test.Controllers
+{
+    public class AuthControllerTests
+    {
+        private ApplicationDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            return new ApplicationDbContext(options);
+        }
+        [Fact]
+        public void Register_ReturnsOkResult_WhenUserIsVaild()
+        {
+            // Arrange
+            var Context = GetDbContext();
+            var controller = new AuthController(Context);
+            var registerDto = new RegisterDto
+            {
+                Nmae = "Dhanvitha",
+                Email = "dhanvitha@gmail.com"
+                Password = "Dhanvitha@123",
+                Role = "Customer"
+            };
+            var result = controller.Register(registerDto);
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void GetUsers_ReturnsOkResult()
+        {
+            // Arrange
+            var Context = GetDbContext();
+            Context.Users.Add(new User
+            {
+                Name = "Ravi",
+                Email = "ravi@gamil.com"
+                Password = "Ravi@123",
+                Role = "Customer"
+            });
+            Context.SaveChanges();
+            var controller = new AuthController(Context);
+            var result = controller.GetUsers();
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void UpdateUser_ReturnsOkResult_WhenUserExists()
+        {
+            // Arrange
+            var Context = GetDbContext();
+            var user = new User
+            {
+                Name = "Raju",
+                Email = "Raju@gamail.com"
+                Password = "Raju@123",
+                Role = "Customer"
+            };
+            context.Users.Add(user);
+            Context.SaveChanges();
+            var controller = new AuthController(Context);
+            var dto = new RegisterDto
+            {
+                Nmae = "Raju Kumar",
+                Email = "rajukumar@gamil.com",
+                Password = "rajuk123",
+                Role = "Admin"
+            };
+            var result = controller.UpdateUser(user.Id, dto);
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void DeleteUser_ReturnsOkResult_WhenUserExists()
+        {
+            // Arrange
+            var Context = GetDbContext();
+            var user = new User
+            {
+                Name = "Raju",
+                Email = "raju@gmail.com"
+                Password = "Raju@123",
+                Role = "Customer"
+            };
+            Context.Users.Add(user);
+            Context.SaveChanges();
+            var controller = new AuthController(Context);
+            var result = controller.DeleteUser(user.Id);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
---
+++ FoodDelivery/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodDelivery.Data;
+using FoodDelivery.Models;
+using FoodDelivery.DTOs;
+
+namespace FoodDelivery.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+    public class AuthController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public AuthController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+        [HttpPost("register")]
+		public IActionResult Register(RegisterDto dto)
+		{
+			var user = new User
+			{
+				Name = dto.Name,
+				Email = dto.Email,
+				Password = dto.Password,
+				Role = dto.Role,
+			};
+			_context.Users.Add(user);
+			_context.SaveChanges();
+			return Ok(new { message = "User registered successfully" });
+		}
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+		[HttpPut("{id}")]
+		public IActionResult UpdateUser(int id,	RegisterDto dto)
+		{
+			var user = _context.Users.Find(id);
+			if (user == null)
+				return NotFound();
+			user.Name = dto.Name;
+			user.Email = dto.Email;
+			user.Password = dto.Password;
+			user.Role = dto.Role;
+			_context.SaveChanges();
+			return Ok("user updated");
+        }
+		[HttpDelete("{id}")]
+		public IActionResult DeleteUser(int id)
+		{
+			var user = _context.Users.Find(id);
+			if (user == null)
+				return NotFound();
+			_context.Users.Remove(users);
+			_context.SaveChanges();
+			return Ok("user deleted");
+        }
+    }
+}
